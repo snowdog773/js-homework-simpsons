d7d7bddbfc02537f87e44f84d1468ba01c6f0f07
Jon Pitans <jonpitans@gmail.com>
2022-02-27 22:48:14 +0000

    first commit

---
+++ simpsons/src/Footer.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <>
+            <span>Copyright 2022 - Jon Pitans Design</span>
+            </>
+        );
+    }
+}
+ 
+export default Footer;
---
+++ simpsons/src/Header.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <>
+                <h1>Welcome to the Simpsons Quote-o-Matic Machine</h1>
+            </>
+        );
+    }
+}
+ 
+export default Header;
